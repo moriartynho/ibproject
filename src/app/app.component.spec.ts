6ea1eca1ef7f481434e1f68cf60f166dda150d43
moriartynho <w1juniorsousa@gmail.com>
2024-12-09 16:54:05 -0300

    test: finalizando configuração de testes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { StoreService } from '../services/store/store.service';
 
+const mockStoreService = {
+  getMusicFromMemory: jest.fn(),
+};
 describe('AppComponent', () => {
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [{ provide: StoreService, useValue: mockStoreService }],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  beforeEach(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    const spyGetMusicFromMemory = jest.spyOn(
+      mockStoreService,
+      'getMusicFromMemory'
+    );
+    expect(component).toBeTruthy();
+    expect(spyGetMusicFromMemory).toHaveBeenCalled();
   });
 });
